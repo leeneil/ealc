c10defac907c25caacd614ea628c4aec1fb77d2e
Ubuntu <ubuntu@ip-172-31-21-78.ec2.internal>
2018-03-01 06:14:25 +0000

    use default python

--- ealc.sh
+++ ealc.sh
@@ -3,12 +3,12 @@
 # --path2-data: subpath of data 
 # --num-data: number of images included in each images
 echo "------------------------ buildLabelCSV.py ------------------------"
-/usr/bin/python3 buildLabelCSV.py --path1-data data --path2-data wikipedia/samples_128 --num-data 100
+python3 buildLabelCSV.py --path1-data data --path2-data wikipedia/samples_128 --num-data 100
 # --test-percentage: the percentage for test set 
 echo "------------------------ buildTFRecord.py ------------------------"
-/usr/bin/python3 buildTFRecord.py --test-percentage 0.2
+python3 buildTFRecord.py --test-percentage 0.2
 # --num-train-steps: number of min-batch traning steps
 # --batch-size: the size of each mini-batch
 # --print-steps: Print the log to screen every other this number of steps.
 echo "------------------------ trainModel.py ------------------------"
-/usr/bin/python3 trainModel.py --num-train-steps 100 --batch-size 10 --print-steps 10
+python3 trainModel.py --num-train-steps 100 --batch-size 10 --print-steps 10
--- ealc_tim.sh
+++ ealc_tim.sh
@@ -4,17 +4,17 @@
 # This run is just to make sure the program runs fine.
 echo "------------------------ timer.py ------------------------"
 # --time: sleep time (unit: hour)
-/usr/bin/python3 timer.py --time 0
+python3 timer.py --time 0
 # --path1-data: path of data
 # --path2-data: subpath of data 
 # --num-data: number of images included in each images
 echo "------------------------ buildLabelCSV.py ------------------------"
-/usr/bin/python3 buildLabelCSV.py --path1-data data --path2-data wikipedia/samples_256 --num-data 30000
+python3 buildLabelCSV.py --path1-data data --path2-data wikipedia/samples_256 --num-data 30000
 # --test-percentage: the percentage for test set 
 echo "------------------------ buildTFRecord.py ------------------------"
-/usr/bin/python3 buildTFRecord.py --test-percentage 0.1
+python3 buildTFRecord.py --test-percentage 0.1
 # --num-train-steps: number of min-batch traning steps
 # --batch-size: the size of each mini-batch
 # --print-steps: Print the log to screen every other this number of steps.
 echo "------------------------ trainModel.py ------------------------"
-/usr/bin/python3 trainModel.py --num-train-steps 1500 --batch-size 100 --print-steps 100
+python3 trainModel.py --num-train-steps 1500 --batch-size 100 --print-steps 100
