3c4d0843f6b8aefbcb1ebecbe0cac21669b06266
Tim <tim_y.chen@yahoo.com>
2018-03-01 02:36:38 -0800

    new job

--- ealc_tim.sh
+++ ealc_tim.sh
@@ -1,13 +1,12 @@
 #!/bin/bash  
-
 echo "------------------------ timer.py ------------------------"
 # --time: sleep time (unit: hour)
 python3 timer.py --time 0
 echo "------------------------ buildLabelCSV.py ------------------------"
 # --path1-data: path of data before "tw"
 # --path2-data: path of data after "tw"
-python3 buildLabelCSV.py --path1-data data --path2-data ted/samples_128 --num-data 600
+python3 buildLabelCSV.py --path1-data data --path2-data ted/samples_128 --num-data 624000
 echo "------------------------ buildTFRecord.py ------------------------"
-python3 buildTFRecord.py --test-percentage 0.1
+python3 buildTFRecord.py --test-percentage 0.05
 echo "------------------------ trainModel.py ------------------------"
-python3 trainModel.py --num-train-steps 100 --batch-size 10 --print-steps 10 --eval-steps 30 --image-size 128
+python3 trainModel.py --num-train-steps 200000 --batch-size 100 --print-steps 1000 --eval-steps 10000 --image-size 128
