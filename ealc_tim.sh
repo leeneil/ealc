6a5c68f18cddf5df99ddc3a82e90a2dffdab2fbd
Tim <tim_y.chen@yahoo.com>
2018-03-01 02:31:12 -0800

    -

--- ealc_tim.sh
+++ ealc_tim.sh
@@ -10,4 +10,4 @@ python3 buildLabelCSV.py --path1-data data --path2-data ted/samples_128 --num-da
 echo "------------------------ buildTFRecord.py ------------------------"
 python3 buildTFRecord.py --test-percentage 0.1
 echo "------------------------ trainModel.py ------------------------"
-python3 trainModel.py --num-train-steps 100 --batch-size 10 --print-steps 10 -- eval-steps 30 --image-size 128
+python3 trainModel.py --num-train-steps 100 --batch-size 10 --print-steps 10 --eval-steps 30 --image-size 128
