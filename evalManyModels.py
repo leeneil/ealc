0dd65c51e0cf692d3271ae9e3c7d42717497f0a8
Tim <tim_y.chen@yahoo.com>
2018-03-02 09:44:56 -0800

    fix bug

--- evalManyModels.py
+++ evalManyModels.py
@@ -9,6 +9,7 @@ def print_confuMat_to_file(file, confuMat):
         for col in row:
             toPrint += "{:0.3f}".format(col) + "\t"
         file.write(toPrint)
+        file.write("\n")
         
         
 ##
@@ -17,24 +18,23 @@ model_root = "optimized_ealc_tensorflow_"
 tfrecords_dir = "tfrecords-output"
 
 ## 
-name_List = [str(i) for i in range(10000,200000,10000)] 
+name_List = [str(i) for i in range(20000,200000,10000)] 
 name_List.append("Final")
 
 ##
 for name in name_List:
     model_name = model_root + name + ".pb"
-    print("========== Eval the model ==========")
-    print("model file: " + os.path.join(model_dir, model_name))
+    print("========== Eval Model: " + os.path.join(model_dir, model_name)+"==========")
     accuracy_percent, accu_confusionMat = evalModel(model_dir, model_name, tfrecords_dir)
     print('accuracy_percent = ', accuracy_percent)
     print(accu_confusionMat)
     print_confuMat(accu_confusionMat)
     
     # write to file 
-    file = open("evalManyModel"+ name +".txt","w")
-    file.write("Model at the step:  " + name) 
-    file.write("Test Set Accuracy: = {:0.5}".format(accuracy_percent))
-    file.write("Confusion Matrix:  " + name)
+    file = open(os.path.join(model_dir, "evalManyModel"+ name +".txt \n"),"w")
+    file.write("Model at the step:  " + name + "\n") 
+    file.write("Test Set Accuracy: = {:0.5} \n".format(accuracy_percent))
+    file.write("Confusion Matrix:  \n")
     print_confuMat_to_file(file, accu_confusionMats)
     file.write("\n")
     file.close()
