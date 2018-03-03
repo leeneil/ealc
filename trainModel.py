c72638ae2d285bf7dc4cd894d189e670ee5b5eff
Tim <tim_y.chen@yahoo.com>
2018-03-03 14:49:07 -0800

    update

--- trainModel.py
+++ trainModel.py
@@ -149,7 +149,7 @@ def evaluate_train_data(sess,x,y,y_,keep_prob,train_data_files, bath_size, corre
     total_correct_preds = 0
     # 4. Define a different tensor operation for summing the correct predictions.
     accuracy2 = tf.reduce_sum(correct_prediction)
-    accu_confusionMat = np.zeros((num_classes,num_classes))
+    confusionMat2 = tf.zeros([num_classes, num_classes], tf.int32)
     for s in range(num_batches):
         # 4A. Accuracy for Train Set
         image_batch2, label_batch2 = sess.run([image_batch, label_batch])
@@ -160,15 +160,16 @@ def evaluate_train_data(sess,x,y,y_,keep_prob,train_data_files, bath_size, corre
         # 4B. Confusion Matrix for Train Set
         train_labels = label_batch2
         train_predictions = sess.run(y, feed_dict={x: image_batch2, y_: label_batch2, keep_prob: 1.0})
-        nature_labels =   np.argmax(train_labels, axis=1)
-        nature_predicts = np.argmax(train_predictions, axis=1)
+        nature_labels =   tf.argmax(train_labels, axis=1)
+        nature_predicts = tf.argmax(train_predictions, axis=1)
         curr_confusionMat = tf.confusion_matrix(labels=nature_labels, predictions=nature_predicts, num_classes=num_classes)
-        accu_confusionMat += sess.run(curr_confusionMat)           
+        confusionMat2 = tf.add(confusionMat2, curr_confusionMat)          
            
     # 5. Record the overall accuracy
     accuracy_percent = total_correct_preds/(num_batches*bath_size)
     print("Overall Training Accuracy {}".format(accuracy_percent))        
     # 6. Record the overall confusion matrix
+    accu_confusionMat = sess.run(confusionMat2) 
     accu_confusionMat = accu_confusionMat/np.sum(accu_confusionMat,axis=1,keepdims=True)
     print('Overall Training Confusion Matrix:')
     print_confuMat(accu_confusionMat)
