55fdfa5652bebb3c3d690bd07d2a6c727ea2cfd6
Tim <tim_y.chen@yahoo.com>
2018-03-12 01:54:50 -0700

    batch size 512

--- ealc_tim.sh
+++ ealc_tim.sh
@@ -9,4 +9,4 @@ python3 buildLabelCSV.py --path1-data data --path2-data ted/samples_128 --num-da
 echo "------------------------ buildTFRecord.py ------------------------"
 python3 buildTFRecord.py --test-percentage 0.05 --test-percentage 0.05
 echo "------------------------ trainModel.py ------------------------"
-python3 trainModel_sm.py --num-train-steps 160000 --batch-size 512 --print-steps 1000 --eval-steps 10000 --image-size 128
+python3 trainModel_sm.py --num-train-steps 30000 --batch-size 512 --print-steps 1000 --eval-steps 3000 --image-size 128
