657a9707167f34ca3bed7ce3ff5e5cc15086b2b1
Tim <tim_y.chen@yahoo.com>
2018-03-12 00:20:42 -0700

    small model

--- ealc_tim.sh
+++ ealc_tim.sh
@@ -7,6 +7,6 @@ echo "------------------------ buildLabelCSV.py ------------------------"
 # --path2-data: path of data after "tw"
 python3 buildLabelCSV.py --path1-data data --path2-data ted/samples_128 --num-data 624000
 echo "------------------------ buildTFRecord.py ------------------------"
-python3 buildTFRecord.py --test-percentage 0.05
+python3 buildTFRecord.py --test-percentage 0.05 --test-percentage 0.05
 echo "------------------------ trainModel.py ------------------------"
-python3 trainModel.py --num-train-steps 200000 --batch-size 100 --print-steps 1000 --eval-steps 10000 --image-size 128
+python3 trainModel_sm.py --num-train-steps 160000 --batch-size 512 --print-steps 1000 --eval-steps 10000 --image-size 128
